7a1529eaf2301634321c188c5ca963213f081377
ssst1986 <tayesukiyakioi4@gmail.com>
2025-10-26 23:18:53 +0900

    streamlit_app.pyを修正：lifespan_textの表示処理など

--- map_logic.py
+++ map_logic.py
@@ -269,6 +269,7 @@ def main2(db_path):
     #filtered = filtered.iloc[:10, :]  # サンプル制限
     # st.write("現在のカラム一覧:", filtered.columns.tolist())
     # ジオコーディング（地理院API）
+    st.write('案件の数は→'len(filtered['full_address']))
     for addr in filtered['full_address']:
         normalized = normalize_address(addr)
         lat, lon = geocode_gsi(normalized)
